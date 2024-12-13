0847ed433ef94e8629ec30abff6d4bf818d71450
lainey1 <lainey.d.paddock@gmail.com>
2024-12-12 21:53:10 -0800

    completed models

--- database.py
+++ database.py
@@ -1,23 +1,98 @@
-from dotenv import load_dotenv
-# Run load_dotenv so environment variables are properly loaded
-load_dotenv()
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
 
 
-from app import app, db
-from app.models import Employee
+class Employee(db.Model, UserMixin):
+    __tablename__ = 'employees'
 
-with app.app_context():
-    # Drop all existing tables
-    db.drop_all()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    employee_number = db.Column(db.Integer, nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
 
-    # Create all tables based on the defined models
-    db.create_all()
+    orders = db.relationship('Order', back_populates='employee', cascade='all, delete-orphan')
 
-    # Check creation
-    print("Tables created!")
-    print(f"Database created at: {app.config['SQLALCHEMY_DATABASE_URI']}")
+    @property
+    def password(self):
+        return self.hashed_password
 
-    # Create an employee
-    employee = Employee(name="Margot", employee_number=1234, password="password")
-    db.session.add(employee)
-    db.session.commit()
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+
+# Menu Model
+class Menu(db.Model):
+    __tablename__ = 'menus'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    items = db.relationship('MenuItem', back_populates='menu', cascade='all, delete-orphan')
+
+# MenuItem Model
+class MenuItem(db.Model):
+    __tablename__ = 'menu_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menus.id'), nullable=False)
+    menu_type_id = db.Column(db.Integer, db.ForeignKey('menu_item_types.id'), nullable=False)
+
+    menu = db.relationship('Menu', back_populates='items')
+    type = db.relationship('MenuItemType', back_populates='items')
+    order_details = db.relationship('OrderDetail', back_populates='menu_item', cascade='all, delete-orphan')
+
+# MenuItemType Model
+class MenuItemType(db.Model):
+    __tablename__ = 'menu_item_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    items = db.relationship('MenuItem', back_populates='type')
+
+
+# Table Model
+class Table(db.Model):
+    __tablename__ = 'tables'
+
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    orders = db.relationship('Order', back_populates='table', cascade='all, delete-orphan')
+
+
+# Order Model
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable=False)
+    table_id = db.Column(db.Integer, db.ForeignKey('tables.id'), nullable=False)
+    finished = db.Column(db.Boolean, nullable=False)
+
+    employee = db.relationship('Employee', back_populates='orders')
+    table = db.relationship('Table', back_populates='orders')
+    details = db.relationship('OrderDetail', back_populates='order', cascade='all, delete-orphan')
+
+    def total_amount(self):
+        return sum(detail.menu_item.price for detail in self.details)
+
+# OrderDetail Model
+class OrderDetail(db.Model):
+    __tablename__ = 'order_details'
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_items.id'), nullable=False)
+
+    order = db.relationship('Order', back_populates='details')
+    menu_item = db.relationship('MenuItem', back_populates='order_details')
